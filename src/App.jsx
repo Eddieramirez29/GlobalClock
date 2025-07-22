791303163aa336a4fdb11ba872c55030832afb9c
Eduardo <tkd13000@gmail.com>
2025-07-22 09:09:00 -0600

    Cards contain information

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,22 @@ import './cards.css';
 function App() {
   return (
     <div className="card-container">
-      {Array.from({ length: 4 }).map((_, i) => (
-        <div className="card" key={i}></div>
-      ))}
+      {Array.from({ length: 4 }).map((_, i) => 
+        (
+          <div className="card" key={i}>
+            <div className="head-card">
+              <div className="country">Mexico</div>
+              <div className="UTC">UTC 0</div>
+            </div>
+            <div className="center">
+              day
+            </div>
+
+            <div className="time">
+              00:00:00
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
